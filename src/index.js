56851783a33a2b039afcfdac227ab719fe803d99
chadalton <chadroyalton@gmail.com>
2018-10-06 15:44:02 -0600

    added initial components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/boostrap.min.css';
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
